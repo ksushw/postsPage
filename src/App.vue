719465895c9a03141d3704d2653d3fda51774ac3
Nevarenyh Kseniya <xenianevarenyh@yandex.ru>
2024-06-01 18:23:17 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import PostList from './components/PostList.vue'
+</script>
+
+<template>
+  <v-app>
+    <v-container>
+      <PostList msg="Vite + Vue" />
+    </v-container>
+  </v-app>
+</template>
+
+
---
+++ src/components/PostList.vue
@@ -0,0 +1,5 @@
+<template>
+   
+        <v-sheet :height="200" :width="200" class="bg-indigo" border></v-sheet>
+      
+  </template> 
