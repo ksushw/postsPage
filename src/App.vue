bc096110f49273c70a002ebbca46186f03a18d87
Nevarenyh Kseniya <xenianevarenyh@yandex.ru>
2024-06-02 23:59:19 +0500

    Create modal page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import PostList from './components/PostList.vue'
+import Modal from './components/Modal.vue'
 </script>
 
 <template>
   <v-app>
     <v-container>
-      <PostList>
-
-      </PostList>
+      <PostList />
+      <Modal title="title" submitTitle="Send" :isOpen="true">
+      </Modal>
     </v-container>
   </v-app>
 </template>
---
+++ src/components/Modal.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+interface Props {
+    isOpen: boolean
+    title: string
+}
+const props = defineProps<Props>()
+</script>
+
+<template>
+    <v-dialog max-width="500" max-height="700" overflow-auto>
+        <!-- butoon delete! -->
+        <template v-slot:activator="{ props: isOpen }">
+            <v-btn v-bind="isOpen" color="surface-variant" text="Open Dialog" variant="flat"></v-btn>
+        </template>
+
+        <template v-slot:default="{ isActive }">
+            <v-card :title="props.title">
+                <v-card-actions>
+                    <v-btn density="default" icon="fas fa-xmark" class="position-absolute top-0 right-0 ma-2 "
+                        @click="isActive.value = false"></v-btn>
+                </v-card-actions>
+                <slot></slot>
+            </v-card>
+        </template>
+    </v-dialog>
+</template>
